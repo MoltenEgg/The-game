20d7f221f29d513d2fe7709d0f74e13f2dd584c8
MoltenEgg <tynyakov97@yandex.ru>
2021-08-12 21:16:25 +0400

    что то рабочее

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ import sys
 pygame.init()
 
 SIZE=WIDTH, HEIGHT=640, 480
+FPS=60
+pygame.display.set_caption("The best game")
 
-screen=pygame
+screen=pygame.display.set_mode(SIZE)
 
-while True:
+def drawing():
+    screen.fill((0, 0, 0))
 
+def main():
+    RUN=True
+    while RUN:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                RUN=False
+                sys.exit()
+        drawing()        
+        pygame.display.update()
 
+if __name__ == '__main__':
+    main()
