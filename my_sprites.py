3cc077fc719215554054c1c57c81baf203b4ed38
MoltenEgg <tynyakov97@yandex.ru>
2021-08-14 21:53:45 +0400

    Добавил спрайты человека для будующих тестов

--- my_sprites.py
+++ my_sprites.py
@@ -3,3 +3,5 @@ import pygame
 goblin=pygame.image.load('assets/goblin.png')
 goblin_dead=pygame.image.load('assets/goblin_dead.png')
 under_enemy=pygame.image.load('assets/under_enemy.png')
+human=pygame.image.load('assets/human.png')
+human_dead=pygame.image.load('assets/human_dead.png')
