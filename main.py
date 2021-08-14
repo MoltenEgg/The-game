c2b8e059f0472b173b8824db9c35e0e0ea163680
MoltenEgg <tynyakov97@yandex.ru>
2021-08-14 21:04:34 +0400

    Добавлены звуки удара и смерти

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ SIZE=WIDTH, HEIGHT=640, 480
 screen=pygame.display.set_mode(SIZE)
 
 
+
 FPS=60
 ENEMY_PLACEMENT=ENEMY_PLACEMENT_x, ENEMY_PLACEMENT_y=(40, 40)
  #тестовое значение здоровья
@@ -20,13 +21,17 @@ ENEMY_PLACEMENT=ENEMY_PLACEMENT_x, ENEMY_PLACEMENT_y=(40, 40)
 
 
 def drawing(Hp, HpMAX):
+    #Переменные
     health_bar_width_stat=64
     health_bar_width_dinamic=health_bar_width_stat*Hp//HpMAX
+    health_font=pygame.font.SysFont('Engravers MT', 10)
+    health_bar_text=health_font.render((str(Hp)+'/'+str(HpMAX)), True, (0, 0, 0))
 
+    #Само рисование
     screen.fill((56, 160, 153))
 
     #Шкала здоровья
-    health_bar=pygame.Rect(ENEMY_PLACEMENT_x, ENEMY_PLACEMENT_y+66, health_bar_width_dinamic, 8)
+    health_bar=pygame.Rect(ENEMY_PLACEMENT_x, ENEMY_PLACEMENT_y+70, health_bar_width_dinamic, 8)
     if health_bar_width_dinamic>=0.7*health_bar_width_stat:
         pygame.draw.rect(screen, (0, 255, 0), health_bar) #Рисуем шкалу здоровья
     elif health_bar_width_dinamic<0.7*health_bar_width_stat and health_bar_width_dinamic>=0.3*health_bar_width_stat:
@@ -34,19 +39,25 @@ def drawing(Hp, HpMAX):
     else:
         pygame.draw.rect(screen, (255, 0, 0), health_bar)
     pygame.draw.rect(screen, (255, 255, 255), (health_bar.x, health_bar.y, health_bar_width_stat, 8), 2)
+    screen.blit(health_bar_text, (health_bar.x+health_bar_width_stat+10, health_bar.y))
 
+    #Под врагом
+    screen.blit(my_sprites.under_enemy, (ENEMY_PLACEMENT_x, ENEMY_PLACEMENT_y+10)) 
     #Рисую гоблина
+    
     if Hp<=0:
         screen.blit(my_sprites.goblin_dead, (ENEMY_PLACEMENT))
     else:
         screen.blit(my_sprites.goblin, (ENEMY_PLACEMENT))
+
     pygame.display.update()
 
 def main():
-    Hp=50
+    Hp=37
     HpMAX=Hp
     RUN=True
-    
+    HIT_SOUND=pygame.mixer.Sound('assets/hit_sound.wav')
+    DYING_SOUND=pygame.mixer.Sound('assets/dying_sound.wav')
     while RUN:
 
         for event in pygame.event.get():
@@ -54,10 +65,15 @@ def main():
                 RUN=False
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == K_a and Hp>0:
-                    Hp-=2
+                if event.key == K_a and Hp>0:                
+                    if Hp-5>0:
+                        HIT_SOUND.play()
+                        Hp-=5
+                    else:
+                        DYING_SOUND.play()
+                        Hp-=5
                 if event.key == K_b and Hp<HpMAX:
-                    Hp+=2
+                    Hp+=5
 
 
         drawing(Hp, HpMAX)        
--- my_sprites.py
+++ my_sprites.py
@@ -2,3 +2,4 @@ import pygame
 
 goblin=pygame.image.load('assets/goblin.png')
 goblin_dead=pygame.image.load('assets/goblin_dead.png')
+under_enemy=pygame.image.load('assets/under_enemy.png')
